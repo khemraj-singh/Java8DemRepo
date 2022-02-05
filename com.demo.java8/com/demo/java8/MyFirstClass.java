a0b9e18100ef526759d5b6294e8e0b3d868004bb
khemraj singh <khem22hmp@gmail.com>
2022-02-05 20:42:00 +0530

    Main method changed in first custom branch java class

--- com.demo.java8/com/demo/java8/MyFirstClass.java
+++ com.demo.java8/com/demo/java8/MyFirstClass.java
@@ -4,6 +4,7 @@ public class MyFirstClass {
 	
 	
 	public static void main(String[] args) {
+		System.out.println("Java class in first custome branch");
 		System.out.println("Hello First Java class");
 	}
 
