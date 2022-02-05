623bcb7f1e58fe77fa146f2acf2cb07349f30a15
khemraj singh <khem22hmp@gmail.com>
2022-02-05 20:30:39 +0530

    Update MyFirstClass.java

--- com.demo.java8/com/demo/java8/MyFirstClass.java
+++ com.demo.java8/com/demo/java8/MyFirstClass.java
@@ -4,6 +4,7 @@ public class MyFirstClass {
 	
 	
 	public static void main(String[] args) {
+	        System.out.println("Class in main branch");
 		System.out.println("Hello First Java class");
 	}
 
