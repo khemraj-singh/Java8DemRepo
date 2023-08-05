2e4b6d988a4a387580a9e8faebac6ae7836895ff
khemraj singh <khem22hmp@gmail.com>
2023-08-05 08:46:21 +0530

    conflict commit

--- com.demo.java8/com/demo/java8/MyFirstClass.java
+++ com.demo.java8/com/demo/java8/MyFirstClass.java
@@ -6,6 +6,7 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("Java class in first custome branch");
 		System.out.println("Hello First Java class");
+		System.out.println("to create conflict");
 	}
 
 }
